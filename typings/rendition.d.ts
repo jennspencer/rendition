f65cb9f508d1af15a90d46d03c4758efb1cdd22b
Dimitrios Lytras <dnlytras@gmail.com>
2018-08-20 17:57:41 +0300

    code-with-copy: Allow to show icon on hover
    Connects-to: #555
    Change-type: minor
    Signed-off-by: Dimitrios Lytras <dimitrios@resin.io>

--- typings/rendition.d.ts
+++ typings/rendition.d.ts
@@ -172,6 +172,7 @@ declare module 'rendition' {
 	interface CodeWithCopyProps extends DefaultProps, Tooltip {
 		copy?: string;
 		text: string;
+		showCopyButton?: 'hover' | 'always';
 	}
 
 	interface CardProps extends DefaultProps {
